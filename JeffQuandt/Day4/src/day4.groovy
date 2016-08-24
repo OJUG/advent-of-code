7681d2a9ef3fc380c170803d42824bd976d86fc8
JeffQuandt <quandt.jeff@gmail.com>
2016-08-24 09:14:50 -0500

    Day 4 solution

---
+++ JeffQuandt/Day4/src/day4.groovy
@@ -0,0 +1,41 @@
+import java.security.MessageDigest
+
+/**
+ * Created by req89790 on 08/24/2016.
+ */
+def key = 'yzbqklnj'
+
+MessageDigest hasher = MessageDigest.getInstance('MD5')
+
+def input = 1
+def md5 = ''
+def coinPtrnP1 = ~/^0{5}/
+def coinPtrnP2 = ~/^0{6}/
+def foundPt1 = false
+def foundPt2 = false
+def coin1
+def coin2
+while (true) {
+
+    md5 = hasher.digest((key + input.toString()).getBytes()).encodeHex().toString()
+
+    if (!foundPt1 && md5.find(coinPtrnP1)) {
+        foundPt1 = true
+        coin1 = input
+    }
+
+    if (!foundPt2 && md5.find(coinPtrnP2)) {
+        foundPt2 = true
+        coin2 = input
+    }
+
+    if (foundPt1 && foundPt2) {
+        break
+    }
+    input++
+}
+
+println "First coin input $coin1"
+println "Second coin input $coin2"
+
+
